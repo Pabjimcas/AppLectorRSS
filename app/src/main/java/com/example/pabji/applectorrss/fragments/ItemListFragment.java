f20bb85628b71e1efee1675435111b20581dbe54
Pablo <pabjimcas@gmail.com>
2016-05-15 10:30:43 +0200

    RecyclerView and Butterknife

--- app/src/main/java/com/example/pabji/applectorrss/fragments/ItemListFragment.java
+++ app/src/main/java/com/example/pabji/applectorrss/fragments/ItemListFragment.java
@@ -3,13 +3,26 @@ package com.example.pabji.applectorrss.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
 import com.example.pabji.applectorrss.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class ItemListFragment extends Fragment {
+
+    @Bind(R.id.recycler_view)
+    RecyclerView recyclerView;
+
+    @Bind(R.id.progress_bar)
+    ProgressBar progressBar;
 
     public static ItemListFragment newInstance() {
         ItemListFragment fragment = new ItemListFragment();
@@ -22,5 +35,25 @@ public class ItemListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_item_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ButterKnife.bind(this, view);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if(getView()!=null){
+            recyclerViewInit();
+        }
+    }
+
+    private void recyclerViewInit() {
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
     }
 }
